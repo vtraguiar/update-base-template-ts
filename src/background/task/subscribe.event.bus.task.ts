3ae51643c288aa9c9f46fb5c586d350fb2fddba4
vtraguiar <jaovitoraguiar@gmail.com>
2024-04-17 09:47:39 -0300

    code working

--- src/background/task/subscribe.event.bus.task.ts
+++ src/background/task/subscribe.event.bus.task.ts
@@ -2,16 +2,11 @@ import { inject, injectable } from 'inversify'
 import { IBackgroundTask } from '../../application/port/background.task.interface'
 import { Identifier } from '../../di/identifiers'
 import { IEventBus } from '../../infrastructure/port/event.bus.interface'
-import { BookDeleteEventHandler } from 'application/integration-event/handler/book.delete.event.handler'
-import { DIContainer } from 'di/di'
-import { ILogger } from 'utils/custom.logger'
-import { BookDeleteEvent } from 'application/integration-event/event/book.delete.event'
 
 @injectable()
 export class SubscribeEventBusTask implements IBackgroundTask {
     constructor(
         @inject(Identifier.RABBITMQ_EVENT_BUS) private readonly _eventBus: IEventBus,
-        @inject(Identifier.LOGGER) private readonly _logger: ILogger
     ) {
     }
 
@@ -34,18 +29,6 @@ export class SubscribeEventBusTask implements IBackgroundTask {
      *  connection, events subscribe is performed.
      */
     private subscribeEvents(): void {
-        this._eventBus
-            .subscribe(new BookDeleteEvent(), new BookDeleteEventHandler(
-                DIContainer.get(Identifier.BOOK_SERVICE),
-                this._logger
-            ),
-                BookDeleteEvent.ROUTING_KEY
-        )
-        .then((result: boolean) => {
-            if (result) this._logger.info('Subscribe in bookDeleteEvent successfull!')
-        })
-        .catch(err => {
-            this._logger.error(`Error in Subscribe Book! ${err.message}`)
-        })
+        // Implement subscribes methods
     }
 }
